fdf8e3d3edbb060f4e6d888751892cfca03f6d55
Riyanbiswas2 <108710712+Riyanbiswas2@users.noreply.github.com>
2022-07-11 20:54:38 +0600

    Create Old.py

---
+++ Old.py
@@ -0,0 +1,13 @@
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip2 install requests')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    
+elif bit == '32bit':
+    from old import menu
+    menu()
